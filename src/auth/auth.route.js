db6b40d3df0200856f149afe5008cd73a1b28c4e
Gustavo Martins <gugumartin77@gmail.com>
2022-04-28 15:41:28 -0300

    Auth login route

---
+++ src/auth/auth.route.js
@@ -0,0 +1,6 @@
+const route = require("express").Router();
+const authController = require("./auth.controller");
+
+route.post("/login", authController.loginController);
+
+module.exports = route;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./database/database");
 const userRoute = require("./users/users.route");
+const authRoute = require("./auth/auth.route");
 
 const port = process.env.PORT || 3005;
 const app = express();
@@ -10,7 +11,8 @@ connectDB();
 
 app.use(express.json());
 app.use(cors());
-app.use('/users', userRoute)
+app.use("/users", userRoute);
+app.use("/auth", authRoute);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
